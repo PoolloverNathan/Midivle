4c047d671e48e62222de3065e49238f9848294ee
PoolloverNathan <nathan.kulzer@protonmail.com>
2022-11-18 12:40:25 +0000

    Serviceworker loader

---
+++ js/swloader.js
@@ -0,0 +1,4 @@
+// DO NOT MODIFY THIS FILE
+// This file will be dynamically created as part of the Github Pages deployment.
+// Modifications to this file will only apply locally.
+// Changes to this file will not be able to be added to the repository.
